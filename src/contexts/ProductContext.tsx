d1443f661c4f78b2c28926d7fd56ca328bda5868
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-04 11:42:46 +0100

    feat: add React contexts for state management

---
+++ src/contexts/ProductContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, FC, ReactNode, useEffect, useState } from "react";
+
+interface ProductProviderProps {
+  children: ReactNode;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+export const ProductContext = createContext<Product[] | null>(null);
+
+const ProductProvider: FC<ProductProviderProps> = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductProvider;
