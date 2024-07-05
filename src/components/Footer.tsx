0868714ec19655b7003ea23fe110291c76099ea1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-05 06:21:23 +0100

    feat: implement footer section
    - Add Footer component with responsive design
    - Include links to social media, contact information, and navigation
    - Add copyright notice and relevant links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,14 @@ const Footer = () => {
     <footer className="bg-primary py-12">
       <div className="container mx-auto">
         <p className="text-white text-center">
-          Copyright &copy; Boluwatife Adeyemi - FinzyStore{" "}
-          {new Date().getFullYear()}. All rights reserved.
+          Copyright &copy;{" "}
+          <a
+            className="border-b"
+            href="https://finzyphinzy.github.io/Portfolio-React/"
+          >
+            Boluwatife Adeyemi
+          </a>
+          - FinzyStore {new Date().getFullYear()}. All rights reserved.
         </p>
       </div>
     </footer>
